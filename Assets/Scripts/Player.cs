743d74eae3316f3bfbf1c38cfe209455bfc48569
Daniele Di Modica <d.dimo94@hotmail.com>
2024-10-30 19:19:45 +0000

    Refactor: Migrate to new Input System
    Replaces the legacy input system with the new input system.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
 
     private bool isWalking;
 
@@ -17,28 +18,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
-
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
         // Apply movement to the trasform
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
@@ -49,8 +31,6 @@ public class Player : MonoBehaviour
         // Lerp for positions, Slerp for rotations
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
-
-        Debug.Log(inputVector);
     }
 
     public bool IsWalking()
