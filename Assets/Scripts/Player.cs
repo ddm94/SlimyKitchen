9b0c1f3951d442e3c01de503e2521543c3147185
Daniele Di Modica <d.dimo94@hotmail.com>
2024-10-30 21:49:48 +0000

    Simple Collision Detection
    Other Changes:
    
    - Adds a floor to the scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,8 +21,47 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        // Apply movement to the trasform
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 1f;
+        float playerHeight = 2f;
+
+        // We are not colliding with an object
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if(!canMove)
+        {
+            // Attempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if(canMove)
+            {
+                // Can move only on the X
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // Attempt only Z movement
+                Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    // Can move only on the X
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    // Cannot move in any direction
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            // Apply movement to the trasform
+            transform.position += moveDir * moveDistance;
+        }
 
         // Check if the player is walking
         isWalking = moveDir != Vector3.zero;
@@ -36,5 +75,10 @@ public class Player : MonoBehaviour
     public bool IsWalking()
     {
         return isWalking;
+    }
+
+    private void OnDrawGizmos()
+    {
+        
     }
 }
