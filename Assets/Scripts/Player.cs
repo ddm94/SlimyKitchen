ae135ccbc4dc38e0b4558e41d0c0849ca1a37952
Daniele Di Modica <d.dimo94@hotmail.com>
2024-10-31 13:50:48 +0000

    Adds Clear Counter
    This is a kitchen prop the player can interact with to perform various actions. For testing purposes, only a very basic implementation has been added so far.
    
    Other Changes:
    
    - Improves project folder structure for better asset organization
    
    Fixes:
    
    - Fixes issue with player animations after renaming the player visual GO

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,8 +6,10 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] LayerMask countersLayerMask;
 
     private bool isWalking;
+    private Vector3 lastInteractDir;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,42 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero )
+        {
+            // Keep track of the last interact movement direction
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+
+        // This way, even if we are not moving, we still have the last interact movement direction and the raycast
+        // will return a hit if it is colliding with something
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                // Has ClearCounter
+                clearCounter.Interact();
+            }
+        }
+    }
+
+    private void HandleMovement()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
@@ -28,13 +66,13 @@ public class Player : MonoBehaviour
         // We are not colliding with an object
         bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
 
-        if(!canMove)
+        if (!canMove)
         {
             // Attempt only X movement
             Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
-            if(canMove)
+            if (canMove)
             {
                 // Can move only on the X
                 moveDir = moveDirX;
@@ -70,11 +108,6 @@ public class Player : MonoBehaviour
         // Lerp for positions, Slerp for rotations
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
-    }
-
-    public bool IsWalking()
-    {
-        return isWalking;
     }
 
     private void OnDrawGizmos()
